98ab4e7a1f029011c7db0672982722cc28c15dfa
feffe1702 <fpm.asd@gmail.com>
2024-12-11 16:03:09 +0100

    [WIP] create card

---
+++ src/components/FilmCard.jsx
@@ -0,0 +1,12 @@
+const FilmCard = ({ title, description }) => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default FilmCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,23 @@
+import FilmCard from '../components/FilmCard';
+
 const Home = () => {
-    return <div className="container">Lista dei film</div>;
+    const films = [
+        { id: 1, title: 'Film 1', description: 'Descrizione Film 1' },
+        { id: 2, title: 'Film 2', description: 'Descrizione Film 2' },
+    ];
+
+    return (
+        <div className="container">
+            <h1>Lista dei film</h1>
+            <div className="row">
+                {films.map((film) => (
+                    <div key={film.id} className="col-md-4">
+                        <FilmCard title={film.title} description={film.description} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
