7fa22d6d9231b474421389f28bcc5a45da92261c
feffe1702 <fpm.asd@gmail.com>
2024-12-11 16:31:08 +0100

    [WIP] add ajax call in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
-import FilmCard from '../components/FilmCard';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
-    const films = [
-        { id: 1, title: 'Film 1', description: 'Descrizione Film 1' },
-        { id: 2, title: 'Film 2', description: 'Descrizione Film 2' },
-    ];
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:3005/movies')
+            .then((response) => response.json())
+            .then((data) => setMovies(data))
+            .catch((error) => console.error('Errore nel recupero dei film:', error));
+    }, []);
 
     return (
         <div className="container">
             <h1>Lista dei film</h1>
             <div className="row">
-                {films.map((film) => (
-                    <div key={film.id} className="col-md-4">
-                        <FilmCard title={film.title} description={film.description} />
+                {movies.map((movie) => (
+                    <div key={movie.id} className="col-md-4  mt-2 mb-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.title}</h5>
+                                <p className="card-text">{movie.abstract}</p>
+                                <a href={`/film/${movie.id}`} className="btn btn-primary">
+                                    Dettagli
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
